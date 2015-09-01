6bd7eca5ffa22e12e3e396c0c2a05faccebb1043
skratchdot <git@skratchdot.com>
2015-09-01 15:00:16 -0400

    please use github-enhancement-suite instead

--- github-repo-filter-info.user.js
+++ github-repo-filter-info.user.js
@@ -4,28 +4,12 @@
 // @description    A user script to display some additional info below the repository filter on a user's main GitHub page.
 // @include        https://github.com/*
 // @match          https://github.com/*
-// @require        https://gist.github.com/skratchdot/5604120/raw/_init.js
-// @require        https://gist.github.com/skratchdot/5604120/raw/d3.ay-pie-chart.js
-// @require        https://gist.github.com/skratchdot/5604120/raw/repo-filter-info.js
 // @run-at         document-end
 // @icon           http://skratchdot.com/favicon.ico
 // @downloadURL    https://github.com/skratchdot/github-repo-filter-info.user.js/raw/master/github-repo-filter-info.user.js
 // @updateURL      https://github.com/skratchdot/github-repo-filter-info.user.js/raw/master/github-repo-filter-info.user.js
-// @version        2.7
+// @version        3.0
 // ==/UserScript==
-/*global SKRATCHDOT, document */
 
-// This code is only going to run for browsers that don't support
-// the @require annotation when executing userscripts.
-if ('undefined' === typeof SKRATCHDOT) {
-	var addScript = function (src) {
-		'use strict';
-		var script = document.createElement('script');
-		script.src = src;
-		document.body.appendChild(script);
-		document.body.removeChild(script);
-	};
-
-	// Required by: repo-filter-info
-	addScript('https://gist.github.com/skratchdot/5604120/raw/repo-filter-info.js');
-}
+console.log('This script has been updated and is part of the Github Enhancement Suite' +
+		' found at: https://github.com/skratchdot/github-enhancement-suite');
